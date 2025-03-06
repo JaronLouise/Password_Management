10af5d021160c6478975ff1298b7bf149f9033fd
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-03-07 02:51:41 +0800

    Defined all unimplemented methods. FINISHED

--- src/User.py
+++ src/User.py
@@ -1,3 +1,5 @@
+from UserData import UserData
+from DataFileHandler import DataFileHandler
 from email_validator import validate_email, EmailNotValidError
 
 
@@ -14,19 +16,19 @@ class User:
         return self.__user_id
 
     @property
-    def email(self):
+    def email(self) -> object:
         return self.__raise_value_error()
 
     @property
-    def password(self):
+    def password(self) -> object:
         return self.__raise_value_error()
 
     @user_id.setter
-    def user_id(self, user_id: str):
+    def user_id(self, user_id: str) -> None:
         self.__user_id = user_id
 
     @email.setter
-    def email(self, email):
+    def email(self, email) -> None:
         try:
             validate_email(email)
             self._email = email
@@ -34,17 +36,45 @@ class User:
             raise ValueError(e)
 
     @password.setter
-    def password(self, password):
+    def password(self, password) -> None:
         self.__password = password
 
-    def signin(self):
-        """ NOT IMPLEMENTED """
+    def signin(self) -> list:
+        file = DataFileHandler()
+        file.filename = "secure-pass_registered-user.txt"
+        file.initialize_file_storage()
 
-    def signup(self):
-        """ NOT IMPLEMENTED """
+        if not file.search("email", self._email):
+            raise ValueError("Email is not registered.")
+
+        if file.select_password(self._email) == self.__password:
+            return file.select_all(self._email)
+
+    def signup(self) -> None:
+        file = DataFileHandler()
+
+        data = self._transform_data()
+        file.filename = "secure-pass_registered-user.txt"
+        file.initialize_file_storage()
+
+        if file.search("email", self._email):
+            raise ValueError("Email is already registered.")
+
+        file.insert(data)
 
     def signout(self):
-        """ NOT IMPLEMENTED """
+        print("Signed-out.")
+        del self
 
-    def __raise_value_error(self):
+    def __raise_value_error(self) -> object:
         raise AttributeError("Cannot access this attribute directly.")
+
+    def _transform_data(self) -> UserData:
+        user: UserData = {
+            "user_id": self.__user_id,
+            "username": self.username,
+            "email": self._email,
+            "password": self.__password,
+            "is_mfa_enabled": self.is_mfa_enabled
+        }
+        return user
