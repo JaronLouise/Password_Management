928437aa954c7a88b1d164319b54ed7b4caa9dec
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-03-07 02:52:13 +0800

    Fixed bugs. FINISHED

--- src/DataFileHandler.py
+++ src/DataFileHandler.py
@@ -1,14 +1,18 @@
+from typing import List
+
 import UserData
-from json import dump
 from pathlib import Path
+from json import dump, load, dumps, JSONDecodeError
 
 
 class DataFileHandler:
     def __init__(self):
         self.filename: str = ""
 
-    def initialize_file_storage(self):
-        if not self.__file_exists():
+    def initialize_file_storage(self) -> None:
+        try:
+            self.__file_exists()
+        except FileNotFoundError:
             file_path = Path(self.filename)
             file_path.touch()
 
@@ -16,20 +20,70 @@ class DataFileHandler:
         self.__file_exists()
 
         try:
+            users: list = self.select_all()
+            users.append(data)
+
             file_path = Path(self.filename)
-            with file_path.open("a", 0, "utf-8") as file:
-                dump(data, file, indent=1)
+            with file_path.open("w", 1, "utf-8") as file:
+                dump(users, file, indent=4)
             return True
         except Exception as e:
             raise OSError(f"Insertion failed: {e}")
 
-    def update(self, user_id: str, data: UserData):
+    def update(self, data: UserData) -> bool:
         self.__file_exists()
+
+        users = self.select_all()
+        for user in users:
+            if user["user_id"] == data["user_id"]:
+                user["username"] = data["username"]
+                user["email"] = data["email"]
+                user["password"] = data["password"]
+                user["is_mfa_enabled"] = data["is_mfa_enabled"]
+
+        file_path = Path(self.filename)
+        try:
+            with file_path.open("w", 0, "utf-8") as file:
+                for user in users:
+                    file.write(dumps(user) + "\n")
+            return True
+        except Exception as e:
+            raise OSError(f"Update failed: {e}")
+
+    def select_all(self, email: str = "") -> List[dict]:
+        file_path = Path(self.filename)
+
+        try:
+            with file_path.open("r", encoding="utf-8") as file:
+                data = load(file)
+
+            if not email:
+                return data
+
+            for d in data:
+                if d["email"] == email:
+                    return [d]
+
+        except JSONDecodeError:
+            return []
+
+    def select_password(self, email) -> str:
+        users = self.select_all()
+
+        for user in users:
+            if user["email"] == email:
+                return user["password"]
+
+    def search(self, key, value) -> bool:
+        users = self.select_all()
+
+        for user in users:
+            if user[key] == value:
+                return True
 
     def __file_exists(self) -> bool:
         file_path = Path(self.filename)
 
         if file_path.is_file() and file_path.exists():
             return True
-        raise FileNotFoundError("Operation failed. The indicated file is not ")
-
+        raise FileNotFoundError("The indicated file does not exists.")
