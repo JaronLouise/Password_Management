f0f7393b2bf0a553e8d57baad8e77eded455c59e
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-03-14 22:13:15 +0800

    fixed home. FINISHED

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -27,14 +27,15 @@ def signup() -> None:
         if password != confirm_password:
             raise Exception("Passwords aren't matching.")
 
+        user.password = password
         user.user_id  = str(uuid4())
 
         user.signup()
         print("Registered successful!")
-        layout_sections("FOOTER")
-
     except Exception as e:
         print(e)
+    finally:
+        layout_sections("FOOTER")
 
 
 def signin() -> int:
@@ -56,7 +57,7 @@ def username_password_signin() -> User:
     try:
         user = User()
         user.username = input("Username: ")
-        user.password = input("Password: ")
+        user.password = pwinput("Password: ", mask="*")
 
         data: list = user.signin()
         if data:
@@ -70,17 +71,28 @@ def username_password_signin() -> User:
             print("Incorrect password.")
     except Exception as e:
         print(e)
+    finally:
         layout_sections("FOOTER")
 
 
-def home():
+def home(user_id: str, username: str, mfa_enabled: bool):
     layout_sections("HEADER", "SecurePass - Home")
+
+    print(f"\n\tUsername: {username}")
+    print(f"\tID: {user_id}\n")
 
     print("\t[1] View stored accounts.")
     print("\t[2] Add new account.")
     print("\t[3] Sync to USB.")
     print("\t[4] Sync from USB.")
+
+    if mfa_enabled: 
+        print("\t[5] Changed multi-factor authentication.")
+    else: 
+        print("\t[5] Enable multi-factor authentication.")
+
     print("\t[0] Sign-out.")
+    layout_sections("BODY")
 
     return int(input("Choice: "))
 
@@ -89,14 +101,14 @@ def layout_sections(section: str, section_name: str = "") -> None:
     length = 52
 
     if section == "HEADER" and section_name:
-        print("\n" * length)
+        print("\033c", end="")
         print('=' * length)
         print(section_name.center(length))
         print('=' * length + "\n")
     elif section == "FOOTER":
         print("\n" + "=" * length)
         input("Press any key to continue.")
-        print("\n" * length)
+        print("\033c", end="")
     elif section == "BODY":
         print(f"\n{'=' * length}")
 
