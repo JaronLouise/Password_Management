3ae637f3eeab97f07d4659ed1a57b11730ee7e92
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-03-14 23:26:19 +0800

    added email and phone number property. FINISHED added a method sending email confirmation. IN PROGRESS

--- src/User.py
+++ src/User.py
@@ -1,14 +1,17 @@
-from Data import UserData
+import sendgrid
+from sendgrid.helpers.mail import Mail
+from Data import UserData, MfaData
 from DataFileHandler import DataFileHandler
-
+from email_validator import validate_email, EmailNotValidError
 
 class User:
     def __init__(self):
         self.__user_id: str = ""
         self.username: str = ""
         self.__password: str = ""
+        self._email: str = ""
+        self._phone_number: str = ""
         self.is_mfa_enabled: bool = False
-        self.mfa_auth_data: UserData
 
     @property
     def user_id(self) -> str:
@@ -17,6 +20,14 @@ class User:
     @property
     def password(self) -> object:
         return self.__raise_value_error()
+    
+    @property
+    def email(self) -> str:
+        return self._email
+    
+    @property
+    def phone_number(self) -> str:
+        return self._phone_number
 
     @user_id.setter
     def user_id(self, user_id: str) -> None:
@@ -25,6 +36,16 @@ class User:
     @password.setter
     def password(self, password) -> None:
         self.__password = password
+
+    @email.setter
+    def email(self, email):
+        try:
+            validate_email(email)
+            self.send_no_reply_email(email)
+
+            self._email = email
+        except EmailNotValidError:
+            raise ValueError("Email is not valid.")
 
     def signin(self) -> list:
         file = DataFileHandler()
@@ -55,15 +76,35 @@ class User:
         print("Signed-out.")
         del self
 
+    def send_no_reply_email(self, email):
+        sg = sendgrid.SendGridAPIClient("your_sendgrid_api_key")
+        email = Mail(
+            from_email="noreply@yourdomain.com",
+            to_emails=email,
+            subject="No-Reply Email",
+            plain_text_content="This is an automated email. Please do not reply."
+        )
+
+        try:
+            response = sg.send(email)
+            print(f"Email sent! Status code: {response.status_code}")
+        except Exception as e:
+            print(f"Failed to send email: {e}")
+
     def __raise_value_error(self) -> object:
         raise AttributeError("Cannot access this attribute directly.")
 
     def _transform_data(self) -> UserData:
+        mfa_data: MfaData = {
+            "email": self._email,
+            "phone_number": self._phone_number
+        }
+
         user: UserData = {
             "user_id": self.__user_id,
             "username": self.username,
             "password": self.__password,
             "is_mfa_enabled": self.is_mfa_enabled,
-            "mfa_auth": {}
+            "mfa_auth": mfa_data
         }
         return user
