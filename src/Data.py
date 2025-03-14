78eb5cd6bca276a7e521d6f8e58dfde047d20c78
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-03-14 21:16:56 +0800

    Renamed UserData.py to Data.py Added class MfaData FINISHED

--- src/Data.py
+++ src/Data.py
@@ -4,7 +4,11 @@ from typing import TypedDict
 class UserData(TypedDict):
     user_id: str
     username: str
-    email: str
     password: str
     is_mfa_enabled: bool
+    mfa_auth: dict
 
+
+class MfaData(TypedDict):
+    email: str
+    contact_number: str
