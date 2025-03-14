36e15a7d027c21e5a482fc297ab953f3a9c0600a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-03-14 21:18:38 +0800

    Modified sign-in. FINISHED

--- src/UserInterface.py
+++ src/UserInterface.py
@@ -1,42 +1,61 @@
 from uuid import uuid4
 from User import User
+from pwinput import pwinput
 
 
 def landing_interface() -> int:
-    print(f"{'=' * 52}\n{' ' * 20} SecurePass {' ' * 20}\n{'=' * 52}\n")
+    layout_sections("HEADER", "SecurePass")
 
     print("\t[1] Signup")
     print("\t[2] Signin")
     print("\t[0] Exit")
 
-    print(f"\n{'=' * 52}")
+    layout_sections("BODY")
 
     return int(input("Choice: "))
 
 
 def signup() -> None:
-    print("\nSIGN-UP")
+    layout_sections("HEADER", "Sign-In")
 
     try:
         user = User()
         user.username = input("Username: ")
-        user.email    = input("Email: ")
-        user.password = input("Password: ")
+        password = pwinput("Password: ", mask="*")
+        confirm_password = pwinput("Confirm Password: ", mask="*")
+
+        if password != confirm_password:
+            raise Exception("Passwords aren't matching.")
+
         user.user_id  = str(uuid4())
 
         user.signup()
         print("Registered successful!")
+        layout_sections("FOOTER")
 
     except Exception as e:
         print(e)
 
 
-def signin() -> User:
-    print("\nSIGN-IN")
+def signin() -> int:
+    layout_sections("HEADER", "Sign-In")
+
+    print("\t[1] Using username and password.")
+    print("\t[2] Using OTP sent via email.")
+    print("\t[3] Using OTP sent via contact number.")
+    print("\t[0] Exit")
+
+    layout_sections("BODY")
+
+    return int(input("Choice: "))
+
+
+def username_password_signin() -> User:
+    layout_sections("HEADER", "Sign-In")
 
     try:
         user = User()
-        user.email = input("Email: ")
+        user.username = input("Username: ")
         user.password = input("Password: ")
 
         data: list = user.signin()
@@ -51,11 +70,11 @@ def signin() -> User:
             print("Incorrect password.")
     except Exception as e:
         print(e)
+        layout_sections("FOOTER")
 
 
 def home():
-    print("\n" * 100)
-    print(f"{'=' * 52}\n{' ' * 20} SecurePass - Home {' ' * 20}\n{'=' * 52}\n")
+    layout_sections("HEADER", "SecurePass - Home")
 
     print("\t[1] View stored accounts.")
     print("\t[2] Add new account.")
@@ -63,6 +82,21 @@ def home():
     print("\t[4] Sync from USB.")
     print("\t[0] Sign-out.")
 
-    print(f"\n{'='*52}")
-
     return int(input("Choice: "))
+
+
+def layout_sections(section: str, section_name: str = "") -> None:
+    length = 52
+
+    if section == "HEADER" and section_name:
+        print("\n" * length)
+        print('=' * length)
+        print(section_name.center(length))
+        print('=' * length + "\n")
+    elif section == "FOOTER":
+        print("\n" + "=" * length)
+        input("Press any key to continue.")
+        print("\n" * length)
+    elif section == "BODY":
+        print(f"\n{'=' * length}")
+
