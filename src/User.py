5f67e388ede686a391bec8c518374c69a8ecff03
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2025-03-22 23:02:23 +0800

    implemented sign in via email otp. FINISHED

--- src/User.py
+++ src/User.py
@@ -1,7 +1,6 @@
-from Data import UserData, MfaData
-from PasswordVault import PasswordVault
+from Data import UserData
 from DataFileHandler import DataFileHandler
-from VerificationService import VerificationService
+from MultiFactorAuth import MultiFactorAuth
 from email_validator import validate_email, EmailNotValidError
 
 class User:
@@ -10,8 +9,8 @@ class User:
         self.username: str = ""
         self.__password: str = ""
         self._email: str = ""
-        self._phone_number: str = ""
         self.is_mfa_enabled: bool = False
+        self._filename = "secure-pass_registered-user.txt"
 
     @property
     def user_id(self) -> str:
@@ -24,10 +23,6 @@ class User:
     @property
     def email(self) -> str:
         return self._email
-    
-    @property
-    def phone_number(self) -> str:
-        return self._phone_number
 
     @user_id.setter
     def user_id(self, user_id: str) -> None:
@@ -42,32 +37,53 @@ class User:
     @email.setter
     def email(self, email):
         self._email = email
-    
-    @phone_number.setter
-    def phone_number(self, phone_num: str) -> None:
-        if not len(phone_num) == 11 and not phone_num.startswith("09"):
-            raise Exception("\t❌ Invalid phone number format.")
-        self._phone_number
 
-    def signin(self) -> list:
+    def signin(self, method) -> bool:
         file = DataFileHandler()
-        file.filename = "secure-pass_registered-user.txt"
+        file.filename = self._filename
         file.initialize_file_storage()
 
-        if not file.search("username", self.username):
-            raise ValueError("User is not registered.")
+        if method == "USERNAME PASSWORD":
+            if not file.search("username", self.username):
+                raise ValueError("User is not registered.")
 
-        stored_password = file.select("username", self.username, "password")
+            stored_password = file.select("username", self.username, "password")
 
-        if stored_password == self.__password:
-            return file.select_all(self.username)
-        
+            return stored_password == self.__password
+            
+        elif method == "EMAIL OTP":
+            if not file.search("email", self.email):
+                raise ValueError("Email is not registed.")
+            
+            mfa = MultiFactorAuth(self.email)
+            otp = mfa.generate_totp()
+
+            email_subject = "SecurePass: One-Time Password (OTP)"
+            email_body = f"""
+                <!DOCTYPE html>
+                <html>
+                <body style="font-family: Arial, sans-serif; line-height: 1.6;">
+                    <h1>Prove Your Signer Identity</h1>
+                    <p>Hello {self.username},</p>
+                    <p>You are required to enter the following code to sign-in in SecurePass. Please enter the code:</p>
+                    <h2 style="background-color: #f4f4f4; padding: 10px; display: inline-block;">{otp}</h2>
+                    <p><i>The request for this access originated from SecurePass.</i></p>
+                </body>
+                </html>
+            """
+            sent = mfa.send_email(email_subject, email_body)
+
+            if not sent: return sent
+
+            i_otp = input("\tOTP: ")
+            return i_otp == otp
+
 
     def signup(self) -> None:
         file = DataFileHandler()
 
         data = self._transform_data()
-        file.filename = "secure-pass_registered-user.txt"
+        file.filename = self._filename
         file.initialize_file_storage()
 
         if file.search("username", self.username):
@@ -83,17 +99,38 @@ class User:
         file = DataFileHandler()
 
         data = self._transform_data()
-        file.filename = "secure-pass_registered-user.txt"
+        file.filename = self._filename
         file.initialize_file_storage()
 
         if file.search("user_id", self.__user_id):
             return file.update(data)
+        
+    def get_user_data(self, method: str):
+        file = DataFileHandler()
+        file.filename = self._filename
+        file.initialize_file_storage()
+
+        if method == "USERNAME PASSWORD":
+            user_data = file.select_all("username", self.username)
+        elif method == "EMAIL OTP":
+            user_data = file.select_all("email", self.email)
+        else: return
+
+        if user_data:
+            self.username       = user_data[0]["username"]
+            self._email         = user_data[0]["email"]
+            self.user_id        = user_data[0]["user_id"]
+            self.__password     = user_data[0]["password"]
+            self.is_mfa_enabled = user_data[0]["is_mfa_enabled"]
 
     def verify_email(self, email: str) -> bool:
         try:
             validate_email(email)
 
-            verification_code = PasswordVault().generate_password(6)
+            mfa: MultiFactorAuth = MultiFactorAuth(email)
+            verification_code = mfa.generate_totp()
+            email_subject = "SecurePass Email Verification"
+
             email_body = f"""
                 <!DOCTYPE html>
                 <html>
@@ -109,33 +146,24 @@ class User:
                 </html>
             """
 
-            VerificationService.send_email(
-                email, 
-                "SecurePass Email Verification", 
-                email_body
-            )
+            sent = mfa.send_email(email_subject, email_body)
+            
+            if not sent: return sent
+            
             i_verification_code = input("\tVerification Code: ")
             return i_verification_code == verification_code
         except EmailNotValidError:
             print("\t❌Error: Email not accepted.")
 
-    def verify_phone_number(self, phone_number: str) -> bool:
-        """ send text message """
-
     def __raise_value_error(self) -> object:
         raise AttributeError("Cannot access this attribute directly.")
 
     def _transform_data(self) -> UserData:
-        mfa_data: MfaData = {
-            "email": self._email,
-            "phone_number": self._phone_number
-        }
-
         user: UserData = {
             "user_id": self.__user_id,
             "username": self.username,
             "password": self.__password,
-            "is_mfa_enabled": self.is_mfa_enabled,
-            "mfa_auth": mfa_data
+            "email": self.email,
+            "is_mfa_enabled": self.is_mfa_enabled
         }
         return user
